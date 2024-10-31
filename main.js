76f68ebff80133cb5d04a41ebfd204414d008954
quyeenpoly <156698407+quyeenpoly@users.noreply.github.com>
2024-10-31 15:43:32 +0700

    Buoi 1 ECMA

---
+++ main.js
@@ -0,0 +1,21 @@
+// ES6 Syntax
+// Biến let, const, var
+var a = 10;
+var a = 20;
+console.log(a);
+// var cho phép thay đổi đè lên
+
+let b = 10;
+b = 20;
+console.log(b);
+// let không cho phép khai báo lại đè lên
+
+const c = 10;
+console.log(c);
+// const là duy nhất không thay đổi
+
+// Khi nào sử dụng const(mặc định dùng), var(không nên dùng), let(khi xác định có giá trị thay đổi)
+
+// Đặt tên biến: camelCase
+// Đặt tên biến mặc định là danh từ vd: myName, myProduct
+//
